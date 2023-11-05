fec1bafdc3de7e343286837282d19c9e56df070c
Truffaconfrutilla <ro.sanhueza@duocuc.cl>
2023-11-05 19:28:05 -0300

    Se agrega config para firebase y el registerUser

---
+++ src/app/config/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC1I428ES7kmHV7PuxBxMf4KY7JlgHguvo",
+    authDomain: "tellevoapp-671ae.firebaseapp.com",
+    projectId: "tellevoapp-671ae",
+    storageBucket: "tellevoapp-671ae.appspot.com",
+    messagingSenderId: "967121726020",
+    appId: "1:967121726020:web:5d08bc66db5c999576ea11",
+    measurementId: "G-EP7VG1BXEJ"
+  };
