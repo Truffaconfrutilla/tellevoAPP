4b7d6d56e73837983b1c074e0303551ec9558f66
Truffaconfrutilla <ro.sanhueza@duocuc.cl>
2023-11-09 17:38:10 -0300

    edicion en user service

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -4,6 +4,9 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, Firestore } from 'firebase/firestore';
 import { User } from '../models/user.model';
 import { FirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
 
 
 //guia ---> https://www.youtube.com/watch?v=HsQGfaY6LBc&list=PL9cZqOuuzAWln7QsiQhpPW7DN2K04MHrL&index=26 
@@ -19,7 +22,9 @@ export class FirebaseService {
 private firestoreDB;
 
 constructor(
-    public database: Firestore
+    public database: Firestore,
+    private auth: AngularFireAuth,
+    public router: Router,
 ) {
     // Initialize Firebase with your configuration
     const app = initializeApp(firebaseConfig);
@@ -39,4 +44,38 @@ async registerUser(userData: User) {
     console.error('Error adding document: ', error);
     }
 }
+
+async login(email: string , pass: string){
+    try{
+        const user = await this.auth.signInWithEmailAndPassword(email,pass);
+        console.log("Se inició sesión de:" + user);
+        if (user){
+            this.router.navigate(['/home']);
+        }
+    };
+}
+
+
+checkLogin() {
+    return new Promise((resolve, reject)=> {
+        this.auth.onAuthStateChanged((user)=> {
+            resolve(user);
+        }, (error)=> {
+            reject(error)
+        });
+    });
+}
+
+async logout() {
+    try {
+        await this.auth.signOut();
+        console.log("SESION CERRADA:");
+        } catch(error) {
+            console.error("Error al cerrar sesion: ", error);
+            }
+        }
+        
+
+
+
 }
