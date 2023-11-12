597691fd18528f6fbc4bb5899b33d7aec322820b
Truffaconfrutilla <ro.sanhueza@duocuc.cl>
2023-11-12 13:21:48 -0300

    register avance

--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { LocationService } from '../../../core/services/location.service'
 import { Location } from '../../../core/models/location.model'
 import Swal from 'sweetalert2';
+import { UserService } from 'src/app/core/services/user.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -10,10 +13,27 @@ import Swal from 'sweetalert2';
 })
 export class RegisterPage implements OnInit {
   locations: Location[] = [];
+  formRegister: FormGroup;
 
   constructor(   
-    private locationService: LocationService
-  ) {}
+    private locationService: LocationService,
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    private toastController: ToastController
+    
+  ) {
+    this.formRegister = this.formBuilder.group({
+      name: ['',[Validators.required]],
+      email: ['',[Validators.required]],
+      password: ['',[Validators.required , Validators.minLength(6)]],
+      password2: ['',[Validators.required , Validators.minLength(6)]],
+      location: ['',[Validators.required]],
+      partner: ['',[Validators.required]],
+      plate: ['',[Validators.required]],
+      licence: ['',[Validators.required]],
+      administrator: ['',[Validators.required]],
+    })
+  }
 
   async loadLocations() {
     try {
@@ -26,5 +46,8 @@ export class RegisterPage implements OnInit {
   ngOnInit() {
     this.loadLocations();
   }
+
+async register(){}
+
 
 }
