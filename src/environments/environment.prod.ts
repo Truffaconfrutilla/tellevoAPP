91ced1e1683c5d2e0c25a9e97bef447a3301ca5f
Truffaconfrutilla <ro.sanhueza@duocuc.cl>
2023-12-02 19:04:18 -0300

    ajuste mapa, ahora se ve error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  googleMapsApiKey: 'AIzaSyALJ1lLCcntz-C3vKIMM0Bs70gi_QU3-g8',
+  googleMapsApiKey: 'AIzaSyBlds-lULNfFRmmdsg4ngivOnCFk9iNiPI',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  googleMapsApiKey: 'AIzaSyALJ1lLCcntz-C3vKIMM0Bs70gi_QU3-g8',
+  googleMapsApiKey: 'AIzaSyBlds-lULNfFRmmdsg4ngivOnCFk9iNiPI',
 
 };
 
