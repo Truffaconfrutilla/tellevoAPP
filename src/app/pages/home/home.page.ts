f1f9edb30ab6f128ee555e75ba9037bd18681702
Truffaconfrutilla <ro.sanhueza@duocuc.cl>
2023-12-05 22:40:00 -0300

    Permisos GPS

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,7 +3,9 @@ import { UserService } from 'src/app/core/services/user.service';
 import { TranslateService } from '@ngx-translate/core';
 import { WeatherService } from 'src/app/core/services/weather.service';
 import { Weather } from 'src/app/core/models/wheater.model';
-import { Geolocation } from '@capacitor/geolocation';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { LocationAccuracy } from '@ionic-native/location-accuracy';
+import { Capacitor } from "@capacitor/core";
 
 @Component({
   selector: 'app-home',
@@ -26,7 +28,7 @@ export class HomePage {
 
   ngOnInit() {
     this.userService.checkLogin()
-    this.requestLocationPermission()
+    this.checkGPSPermission()
     this.weatherService.getWeather().subscribe(
       (data) => {
         this.weather = data
@@ -37,18 +39,72 @@ export class HomePage {
     );
   }
 
-  async requestLocationPermission() {
-    const permission = await Geolocation.requestPermissions();
-    if (permission && permission.location === 'granted') {
-    } else {
-      console.warn('Location permission denied.');
-    }
-  }
-
-
-changeLang(event:any) {
+  changeLang(event:any) {
     this.translateService.use(event.detail.value);
   }
 
+  async askToTurnOnGPS(): Promise<boolean> {
+    return await new Promise((resolve, reject) => {
+        LocationAccuracy.canRequest().then((canRequest: boolean) => {
+            if (canRequest) {
+                // the accuracy option will be ignored by iOS
+                LocationAccuracy.request(LocationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
+                    () => {
+                        resolve(true);
+                    },
+                    error => { resolve(false); }
+                );
+            }
+            else { resolve(false); }
+        });
+    })
+  }
+
+  async checkGPSPermission(): Promise<boolean> {
+    return await new Promise((resolve, reject) => {
+        if (Capacitor.isNative) {
+            AndroidPermissions.checkPermission(AndroidPermissions.PERMISSION.ACCESS_FINE_LOCATION).then(
+                result => {
+                    if (result.hasPermission) {
+                        // If having permission show 'Turn On GPS' dialogue
+                        resolve(true);
+                    } else {
+                        // If not having permission ask for permission
+                        resolve(false);
+                    }
+                },
+                err => { alert(err); }
+            );
+        }
+        else { resolve(true); }
+    })
+  }
+
+  async requestGPSPermission(): Promise<string> {
+    return await new Promise((resolve, reject) => {
+        LocationAccuracy.canRequest().then((canRequest: boolean) => {
+            if (canRequest) {
+                resolve('CAN_REQUEST');
+            } else {
+                // Show 'GPS Permission Request' dialogue
+                AndroidPermissions.requestPermission(AndroidPermissions.PERMISSION.ACCESS_FINE_LOCATION)
+                    .then(
+                        (result) => {
+                            if (result.hasPermission) {
+                                // call method to turn on GPS
+                                resolve('GOT_PERMISSION');
+                            } else {
+                                resolve('DENIED_PERMISSION');
+                            }
+                        },
+                        error => {
+                            // Show alert if user click on 'No Thanks'
+                            alert('requestPermission Error requesting location permissions ' + error);
+                        }
+                    );
+            }
+        });
+    })
+}
 
 }
